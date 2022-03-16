445438319bde59443dbc32543870adda07794728
nlbien22 <92375489+nlbien22@users.noreply.github.com>
2022-03-16 17:24:10 +0700

    Tong Hop Code

---
+++ JAVA/src/main/java/com/mycompany/java/JAVA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.java;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author LBIT
+ */
+public class JAVA {
+
+    public static void main(String[] args) {
+        LBIT a = new LBIT();
+        a.show(true);
+    }
+}
